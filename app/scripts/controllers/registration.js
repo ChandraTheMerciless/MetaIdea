62b680121f030dc7ec48d1b9a345711872cc1042
Chandra Asar <casar@icct.com>
2015-11-22 17:49:20 -0500

    yay done?

--- app/scripts/controllers/registration.js
+++ app/scripts/controllers/registration.js
@@ -18,36 +18,39 @@ angular.module('metaideaApp')
       password:""
     };
 
-  $scope.verifyPassword = {
-    password: ""
-  };
+//  $scope.verifyPassword = {
+//    password: ""
+//  };
+  
+  //$scope.validPwds=true;
+  $scope.registerSuccess = false;
 
     $scope.newUserReg = function(){
-      $scope.verifyPasswords();
+  //    $scope.verifyPasswords();
+  //    if($scope.validPwds == true){
+        service.register($scope.newUser).then(function(user){
+              console.log('user created successfully');
+              $location.path('/login');
+  //            service.login(user).then(function(){
+  //                $location.path('/home');
+  //            });
 
-      service.register($scope.newUser).then(function(user){
-            console.log('user created successfully');
-            $location.path('/login');
-//            service.login(user).then(function(){
-//                $location.path('/home');
-//            });
-
-        });
+          });
+      $scope.registerSuccess = true;
+   //   };
     };
 
-    $scope.validPwds=true;
-
-    $scope.verifyPasswords = function(){
-      console.log($scope.newUser.password);
-      console.log($scope.verifyPassword.password);
-
-      var pwd = $scope.newUser.password;
-      var verifyPwd = $scope.verifyPassword.password;
-
-      if(pwd == verifyPwd){
-        $scope.validPwds=true;
-      } else {
-        $scope.validPwds=false;
-      };
-    };
+//    $scope.verifyPasswords = function(){
+//      console.log($scope.newUser.password);
+//      console.log($scope.verifyPassword.password);
+//
+//      var pwd = $scope.newUser.password;
+//      var verifyPwd = $scope.verifyPassword.password;
+//
+//      if(pwd == verifyPwd){
+//        $scope.validPwds=true;
+//      } else {
+//        $scope.validPwds=false;
+//      };
+//    };
   });
--- test/spec/controllers/problem.js
+++ test/spec/controllers/problem.js
@@ -5,22 +5,66 @@ describe('Controller: ProblemCtrl', function () {
   // load the controller's module
   beforeEach(module('metaideaApp'));
 
-  var ProblemCtrl,
+  var ProblemCtrl,$q,
     scope;
+  
+  var service = {
+    getProblem:function(problem){
+      var def = $q.defer();
+      return promise;
+  },
+    updateDescription:function(item, description){
+      var def = $q.defer();
+      return promise;
+    }
+  };
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, service, $routeParams, _$q_) {
+    $q = _$q_;
+    spyOn(service, 'getProblem').andReturn($q.when('ss'));
+    spyOn(service, 'updateDescription').andReturn($q.when('ss'));
+    spyOn(service, 'createComment').andReturn($q.when('ss'));
+    
+    
     scope = $rootScope.$new();
-    ProblemCtrl = $controller('ProblemCtrl', {
-      $scope: scope
-      
-      // place here mocked dependencies
+      ProblemCtrl = $controller('ProblemCtrl', {
+      $scope: scope,
+      controller: $controller,
+      service: service,
+      routeParams: $routeParams
     });
   }));
 
-  it('should call the function on click submit', function () {
-    $scope.submitComment();
+  it('should expect edit mode to be false on initialization', function () {
     
-    //expect(ProblemCtrl.awesomeThings.length).toBe(3);
+    expect(scope.editMode).toBe(false);
+
   });
+  
+  it('should expect editText function to change local editMode to true', function(){
+    var item = {editMode : false};
+    scope.editText(item);
+    expect(item.editMode).toBe(true);
+  });
+  
+  
+  it('should expect saveText function to change local editMode to false', function(){
+    var item = {editMode : true};
+    scope.problem = {description:"data"};
+    scope.saveText(item);
+    expect(item.editMode).toBe(false);
+  })
+  
+  it('should expect createComment in service to be called on addComment', function(){
+    scope.problem = {comments : []};
+//    spyOn();
+    
+    scope.addComment();
+    
+    expect(service.createComment).toHaveBeenCalled;
+    
+    
+  })
+  
 });
